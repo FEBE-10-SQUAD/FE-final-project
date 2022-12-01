8ef27453241857d65fe6f02602107fcc420ad89e
anisyakhanza <anisyakhanza22@gmail.com>
2022-12-01 14:00:28 +0700

    tampilan status page

---
+++ src/components/Status/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Seacrh = () => {
+  return (
+    <div className='seacrh'>
+        <input type="text" placeholder="Job Title or Keyword" value={Seacrh}/>
+        <button >FIND</button>
+    </div>
+  )
+}
+
+export default Seacrh
